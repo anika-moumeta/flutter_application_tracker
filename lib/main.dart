6a869bc3b60fb962c03a37f08b06cdb664da3d60
anika-moumeta <anikaafrin3344@gmail.com>
2025-04-02 21:25:27 +0600

    Problem5

--- lib/main.dart
+++ lib/main.dart
@@ -2,82 +2,103 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(const StyledTextApp());
+  runApp(const NavigationApp());
 }
 
-class StyledTextApp extends StatelessWidget {
-  const StyledTextApp({super.key});
+class NavigationApp extends StatelessWidget {
+  const NavigationApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const TextStyleScreen(),
+      home: const FirstScreen(),
     );
   }
 }
 
-class TextStyleScreen extends StatelessWidget {
-  const TextStyleScreen({super.key});
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              'This is a Text Widget',
-              textAlign: TextAlign.center,
-              style: GoogleFonts.tinos(
-                textStyle: const TextStyle(
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
-                  color: Color.fromARGB(255, 12, 5, 110),
-                ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('assets/images/rain.jpg'),
+          Text(
+            "This is First Screen",
+            style: TextStyle(
+              fontFamily: GoogleFonts.lato().fontFamily,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: const Color.fromARGB(255, 203, 10, 74),
+            ),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const SecondScreen()),
+              );
+            },
+            child: const Text('Go to Second Screen'),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color.fromARGB(255, 214, 205, 202),
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              textStyle: TextStyle(
+                fontFamily: GoogleFonts.lato().fontFamily,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: const Color.fromARGB(255, 203, 10, 74),
               ),
             ),
-            const SizedBox(height: 10),
-            Text(
-              'Created By Me',
-              textAlign: TextAlign.center,
-              style: GoogleFonts.emilysCandy(
-                textStyle: const TextStyle(
-                  fontSize: 25,
-                  fontWeight: FontWeight.w700,
-                  fontStyle: FontStyle.italic,
-                  color: Color.fromARGB(255, 144, 45, 149),
-                ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('assets/images/tree.jpg'),
+          Text(
+            "This is Second Screen",
+            style: TextStyle(
+              fontFamily: GoogleFonts.lato().fontFamily,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: const Color.fromARGB(255, 203, 10, 74),
+            ),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('Go to First Screen'),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color.fromARGB(255, 222, 215, 212),
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              textStyle: TextStyle(
+                fontFamily: GoogleFonts.lato().fontFamily,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: const Color.fromARGB(255, 25, 12, 205),
               ),
             ),
-            const SizedBox(height: 10),
-            Text(
-              'A text with Underline',
-              textAlign: TextAlign.center,
-              style: GoogleFonts.kings(
-                textStyle: const TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.w800,
-                  decoration: TextDecoration.underline,
-                  color: Color.fromARGB(255, 164, 156, 8),
-                ),
-              ),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              'Thank you for using my app',
-              textAlign: TextAlign.center,
-              style: GoogleFonts.kranky(
-                textStyle: const TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w900,
-                  color: Color.fromARGB(255, 135, 18, 10),
-                ),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
