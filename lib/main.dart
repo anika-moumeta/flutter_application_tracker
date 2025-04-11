e59ae0fee6464b340b9ad3ca546ec02a62564e50
anika-moumeta <anikaafrin3344@gmail.com>
2025-04-11 14:40:10 +0600

    Problem7

--- lib/main.dart
+++ lib/main.dart
@@ -9,26 +9,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-      theme: ThemeData(
-        inputDecorationTheme: InputDecorationTheme(
-          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: const Color.fromARGB(255, 159, 28, 128),
-            foregroundColor: Colors.white,
-            elevation: 2,
-            fixedSize: const Size.fromWidth(double.maxFinite),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20),
-            ),
-          ),
-        ),
-      ),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
   }
 }
 
@@ -40,66 +21,57 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final TextEditingController _email = TextEditingController();
-  final TextEditingController _password = TextEditingController();
-  final _formKey = GlobalKey<FormState>();
-
-  void _onPressedLoginButton() {
-    if (_formKey.currentState!.validate()) {
-      ScaffoldMessenger.of(
-        context,
-      ).showSnackBar(SnackBar(content: Text("Login Successfull")));
-    }
-  }
-
+  final List<String> imageLinks = [
+    "https://onestep4ward.com/wp-content/uploads/2011/08/dreamstime_xxl_55742473-1536x1207.jpg",
+    "https://cdn.dhakapost.com/media/imgAll/BG/2021October/bagerhat-20211017170638.jpg",
+    "https://cdn.dhakapost.com/media/imgAll/BG/2021October/sreemangal-20211017170532.jpg",
+    "https://cdn.dhakapost.com/media/imgAll/BG/2021October/kuakata-20211017170353.jpg",
+    "https://cdn.dhakapost.com/media/imgAll/BG/2021October/tanguar-20211017170319.jpg",
+    "https://cdn.dhakapost.com/media/imgAll/BG/2021October/cover-20211017171031.jpg",
+    "https://th.bing.com/th/id/OIP.Sylw2IWh00Y_9jNiEq9N6gHaE_?w=266&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    "https://th.bing.com/th/id/OIP.GYc0OVKYtf7WhYgfVbyYNwAAAA?rs=1&pid=ImgDetMain",
+    "https://nijhoom.com/wp-content/uploads/2021/03/sonargaon-museum-boro-sardar-bari-hall-768-o-1.jpg",
+    "https://th.bing.com/th/id/R.babdffcb77c5cee460d899d118e56d10?rik=Y1Zp8E9q0G1P3g&pid=ImgRaw&r=0",
+    "https://th.bing.com/th/id/R.1a9623303e1d6b087dfcf4e3d99d43f8?rik=jtD%2bMxD4F7O8%2fw&pid=ImgRaw&r=0",
+    "https://cinebuzztimes.com/wp-content/uploads/2022/04/paharpur.jpg",
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         foregroundColor: Colors.white,
-        backgroundColor: const Color.fromARGB(255, 159, 28, 128),
+        backgroundColor: const Color.fromARGB(255, 221, 49, 58),
         title: Center(
           child: Text(
-            "Login page",
+            "Places To Visit In BD",
             style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
           ),
         ),
       ),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              customTextFromField(hintText: "Enter Email", controller: _email),
-              const SizedBox(height: 30),
-              customTextFromField(hintText: "Password", controller: _password),
-              const SizedBox(height: 30),
-              ElevatedButton(
-                onPressed: _onPressedLoginButton,
-                child: Text("Log In"),
-              ),
-            ],
+        padding: const EdgeInsets.all(12.0),
+        child: GridView.builder(
+          itemCount: imageLinks.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 1,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
           ),
+          itemBuilder: (context, index) {
+            return Container(
+              //height: 400,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                image: DecorationImage(
+                  image: NetworkImage(imageLinks[index]),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            );
+          },
         ),
       ),
-    );
-  }
-
-  TextFormField customTextFromField({
-    required String hintText,
-    required TextEditingController controller,
-  }) {
-    return TextFormField(
-      decoration: InputDecoration(hintText: hintText),
-      validator: (String? value) {
-        if (value?.trim().isEmpty ?? true) {
-          return "Must Enter $hintText ";
-        }
-        return null;
-      },
-      controller: controller,
     );
   }
 }
