d61e12b151a2fed46f22a2dccc0514f29fe5b14b
anika-moumeta <anikaafrin3344@gmail.com>
2025-04-11 18:31:52 +0600

    Problem11

--- lib/main.dart
+++ lib/main.dart
@@ -1,135 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
-void main() {
+main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: GridViewScreen(),
       debugShowCheckedModeBanner: false,
-      home: BottomNavScreen(),
     );
   }
 }
 
-class BottomNavScreen extends StatelessWidget {
+class GridViewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Center(
-            child: Column(
-              children: [
-                SizedBox(height: 10),
-                Text(
-                  'Animal Lovers',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.indigo,
-                    fontSize: 25,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-        body: TabBarView(
-          physics: NeverScrollableScrollPhysics(), // Prevents swipe navigation
-          children: [HomeScreen(), AnimalScreen(), ProfileScreen()],
-        ),
-        bottomNavigationBar: TabBar(
-          labelColor: const Color.fromARGB(255, 18, 95, 157),
-          unselectedLabelColor: Colors.grey,
-          indicatorColor: Colors.transparent, // Removes indicator animation
-          tabs: [
-            Tab(icon: Icon(Icons.home), text: 'Home'),
-            Tab(icon: Icon(Icons.camera), text: 'Animals'),
-            Tab(icon: Icon(Icons.person), text: 'Profile'),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'Home Screen',
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            "Tracker Sheet - Local Image",
             style: TextStyle(
+              color: Colors.white,
               fontSize: 20,
               fontWeight: FontWeight.bold,
-              color: Colors.brown,
             ),
           ),
-          SizedBox(height: 10),
-          Image.asset('assets/images/cat1.jpg', width: 550, height: 500),
-        ],
+        ),
+        backgroundColor: const Color.fromARGB(255, 14, 80, 113),
       ),
-    );
-  }
-}
-
-class AnimalScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'Wild Animals',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Colors.brown,
-            ),
-          ),
-          SizedBox(height: 10),
-          Image.asset('assets/images/tiger.jpg', width: 550, height: 500),
-        ],
-      ),
-    );
-  }
-}
-
-class ProfileScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          CircleAvatar(
-            backgroundImage: AssetImage("assets/images/girl.png"),
-            radius: 150,
-          ),
-          SizedBox(height: 8),
-          Text(
-            'Anika Afrin Moumeta',
-            style: TextStyle(
-              fontSize: 25,
-              fontWeight: FontWeight.bold,
-              color: const Color.fromARGB(255, 218, 93, 93),
-              fontFamily: GoogleFonts.amarante().fontFamily,
-            ),
-          ),
-          Text(
-            'Student | Daffodil International University | Animal Lover',
-            style: TextStyle(fontSize: 15),
-          ),
-        ],
-      ),
+      body: Center(child: Image.asset("assets/images/cat1.jpg")),
     );
   }
 }
