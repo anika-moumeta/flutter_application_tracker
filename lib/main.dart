7619eb2aef4f502541743580afcd4d936eaf9227
anika-moumeta <anikaafrin3344@gmail.com>
2025-04-11 18:50:28 +0600

    Problem12

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,69 @@
 import 'package:flutter/material.dart';
 
-main() {
-  runApp(MyApp());
+void main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: GridViewScreen(),
       debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Padding(
+            padding: const EdgeInsets.only(top: 40),
+            child: Center(
+              child: Text(
+                'Tracker Sheet - Custom Container',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          backgroundColor: Color.fromARGB(255, 160, 89, 89),
+        ),
+        backgroundColor: const Color.fromARGB(255, 160, 89, 89),
+        body: const Center(child: CustomContainer()),
+      ),
     );
   }
 }
 
-class GridViewScreen extends StatelessWidget {
+class CustomContainer extends StatelessWidget {
+  const CustomContainer({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Center(
-          child: Text(
-            "Tracker Sheet - Local Image",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
+    return Container(
+      width: 300,
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: const Color.fromARGB(255, 5, 62, 108),
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.3),
+            blurRadius: 10,
+            spreadRadius: 2,
+            offset: Offset(5, 5),
           ),
-        ),
-        backgroundColor: const Color.fromARGB(255, 14, 80, 113),
+        ],
       ),
-      body: Center(child: Image.asset("assets/images/cat1.jpg")),
+      child: const Text(
+        'Custom Container With Text Widget!',
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          fontSize: 25,
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
     );
   }
 }
