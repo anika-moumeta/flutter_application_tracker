5683bc75fd54179f7895dee934e2253691f27af8
anika-moumeta <anikaafrin3344@gmail.com>
2025-04-12 03:15:56 +0600

    Problem16

--- lib/main.dart
+++ lib/main.dart
@@ -10,126 +10,65 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Date & Time Selector',
-      home: const DateTimePickerScreen(),
+      title: 'Tracker Sheet',
+      home: AnimatedContainerExample(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class DateTimePickerScreen extends StatefulWidget {
-  const DateTimePickerScreen({super.key});
+class AnimatedContainerExample extends StatefulWidget {
   @override
-  DateTimePickerScreenState createState() => DateTimePickerScreenState();
+  _AnimatedContainerExampleState createState() =>
+      _AnimatedContainerExampleState();
 }
 
-class DateTimePickerScreenState extends State<DateTimePickerScreen> {
-  DateTime? selectedDate;
-  TimeOfDay? selectedTime;
-
-  Future<void> _pickDate(BuildContext context) async {
-    final DateTime? picked = await showDatePicker(
-      context: context,
-      initialDate: selectedDate ?? DateTime.now(),
-      firstDate: DateTime(1900),
-      lastDate: DateTime(2500),
-    );
-    if (picked != null && picked != selectedDate) {
-      setState(() {
-        selectedDate = picked;
-      });
-    }
-  }
-
-  Future<void> _pickTime(BuildContext context) async {
-    final TimeOfDay? picked = await showTimePicker(
-      context: context,
-      initialTime: selectedTime ?? TimeOfDay.now(),
-    );
-    if (picked != null && picked != selectedTime) {
-      setState(() {
-        selectedTime = picked;
-      });
-    }
-  }
+class _AnimatedContainerExampleState extends State<AnimatedContainerExample> {
+  bool _isChanged = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 249, 201, 201),
+      backgroundColor: const Color.fromARGB(255, 246, 146, 255),
       appBar: AppBar(
-        title: Center(
-          child: Column(
-            children: [SizedBox(height: 30), Text('Date & Time Selector')],
-          ),
+        title: Column(
+          children: [
+            SizedBox(height: 30),
+            Text(
+              'Animated Container',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+            ),
+          ],
         ),
-        titleTextStyle: TextStyle(
-          fontSize: 25,
-          fontWeight: FontWeight.bold,
-          color: const Color.fromARGB(255, 0, 0, 0),
-        ),
-        backgroundColor: const Color.fromARGB(255, 249, 201, 201),
+        backgroundColor: const Color.fromARGB(255, 246, 146, 255),
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: () => _pickDate(context),
-                child: const Text(
-                  'Pick a Date',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color.fromARGB(255, 190, 27, 27),
-                  elevation: 5,
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 20,
-                    vertical: 10,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () => _pickTime(context),
-                child: const Text(
-                  'Pick Time',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color.fromARGB(255, 190, 27, 27),
-                  elevation: 5,
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 20,
-                    vertical: 10,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 40),
-              Text(
-                selectedDate == null
-                    ? 'No Date is selected'
-                    : 'Selected Date: ${selectedDate?.toLocal()}',
-                style: const TextStyle(fontSize: 18),
-              ),
-              Text(
-                selectedTime == null
-                    ? 'No Time is selected'
-                    : 'Selected Time: ${selectedTime?.format(context)}',
-                style: const TextStyle(fontSize: 18),
-              ),
-            ],
+
+      body: Center(
+        child: AnimatedContainer(
+          duration: Duration(seconds: 1),
+          curve: Curves.easeInOut,
+          width: _isChanged ? 300 : 200,
+          height: _isChanged ? 300 : 200,
+
+          decoration: BoxDecoration(
+            color:
+                _isChanged
+                    ? const Color.fromARGB(255, 117, 17, 17)
+                    : const Color.fromARGB(255, 24, 87, 139),
+            borderRadius: BorderRadius.circular(_isChanged ? 100 : 0),
           ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _isChanged = !_isChanged;
+          });
+        },
+        child: Icon(Icons.play_arrow),
+        backgroundColor: const Color.fromARGB(255, 16, 61, 97),
+        foregroundColor: Colors.white,
       ),
     );
   }
